b1053114be147796018dcb3eb3a9c855ee615cf7
ziadMohamed00 <ziadelgammal123@gmail.com>
2024-01-18 04:53:08 -0800

    0x02

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
-if __name__ == "__main__"
-    """print all hidden directories"""
+
+if __name__ == "__main__":
+    """Print all names defined by hidden_4 module."""
     import hidden_4
 
-    for i in dir(hidden_4):
-        if i[:2] != "__":
-            print(i)
+    names = dir(hidden_4)
+    for name in names:
+        if name[:2] != "__":
+            print(name)
