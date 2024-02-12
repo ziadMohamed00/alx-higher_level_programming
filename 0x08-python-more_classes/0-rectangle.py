71499a9f5d527c3bfbfae3b7996a761cd2c51e97
ziadMohamed00 <ziadelgammal123@gmail.com>
2024-02-12 13:17:48 -0800

    init commit

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+This is the "Rectangle"  module.
+
+This module provides a simple Rectangle class.
+"""
+
+
+class Rectangle:
+    """A simple Rectangle class"""
+    pass#!/usr/bin/python3
+"""
+This is the "Rectangle"  module.
+
+This module provides a simple Rectangle class.
+"""
+
+
+class Rectangle:
+        """A simple Rectangle class"""
+            pass#!/usr/bin/python3
+        """
+        This is the "Rectangle"  module.
+
+        This module provides a simple Rectangle class.
+        """
+
+
+        class Rectangle:
+                """A simple Rectangle class"""
+                    pass
