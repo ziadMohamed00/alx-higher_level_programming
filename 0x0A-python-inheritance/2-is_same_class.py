c15080fe01817429c3c8c968a154bc8256bc6b72
ziadMohamed00 <ziadelgammal123@gmail.com>
2024-02-19 13:21:11 -0800

    init commit

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,12 +1,17 @@
 #!/usr/bin/python3
-"""
-================================
-module with method is_same_class
-================================
-"""
+"""Defines a class-checking function."""
 
 
 def is_same_class(obj, a_class):
-    """Method that return True if an object is an instance of a class"""
+    """Check if an object is exactly an instance of a given class.
 
-    return type(obj) is a_class
+    Args:
+        obj (any): The object to check.
+        a_class (type): The class to match the type of obj to.
+    Returns:
+        If obj is exactly an instance of a_class - True.
+        Otherwise - False.
+    """
+    if type(obj) == a_class:
+        return True
+    return False
--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,13 +1,17 @@
 #!/usr/bin/python3
-"""
-===================================
-module with method inherits_from
-===================================
-"""
+"""Defines an inherited class-checking function."""
 
 
 def inherits_from(obj, a_class):
-    """Method that return True if an object is an instance of a class
-    that inherited from"""
+    """Checks if an object is an inherited instance of a class.
 
-    return False if type(obj) is a_class else isinstance(obj, a_class)
+    Args:
+        obj (any): The object to check.
+        a_class (type): The class to match the type of obj to.
+    Returns:
+        If obj is an inherited instance of a_class - True.
+        Otherwise - False.
+    """
+    if issubclass(type(obj), a_class) and type(obj) != a_class:
+        return True
+    return False
