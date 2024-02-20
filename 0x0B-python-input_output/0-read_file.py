5b15b9fe8e0c20a40233337ca30fefc0eca30e4a
ziadMohamed00 <ziadelgammal123@gmail.com>
2024-02-20 12:02:22 -0800

    commit

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
-"""That reads a text file (UTF8) and prints it to stdout."""
+"""Defines a text file-reading function."""
+
 
 def read_file(filename=""):
-    with open(filename, "r", encoding="UTF-8") as f:
+    """Print the contents of a UTF8 text file to stdout."""
+    with open(filename, encoding="utf-8") as f:
         print(f.read(), end="")
